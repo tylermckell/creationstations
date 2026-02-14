07644f91a4450d4fb86cfa2c12593990b13ef28a
Tyler McKell <106923997+tylermckell@users.noreply.github.com>
2026-02-14 07:42:11 -0800

    Add files via upload

---
+++ AppMatcher/Assets/Javascript/sidenav.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+<noscript>
+Your browser does not support Javascript. Please update your browser or use Google Chrome
+</noscript>
